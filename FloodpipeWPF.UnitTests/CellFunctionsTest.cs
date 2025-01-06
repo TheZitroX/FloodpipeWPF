d3bf2d73f4c19bb873d07d008077828c37fc022a
John Lienau <develop@johnlienau.de>
2025-01-06 03:58:43 +0100

    adds CellTests

--- FloodpipeWPF.UnitTests/CellFunctionsTest.cs
+++ FloodpipeWPF.UnitTests/CellFunctionsTest.cs
@@ -4,12 +4,117 @@ using System.Numerics;
 namespace FloodPipeWPF.UnitTests
 {
     [TestClass]
+    public sealed class CellTest
+    {
+        [TestMethod]
+        public void CreateCell_EmptyCell_EmptyCell()
+        {
+            var cell = new Cell(CellType.EMPTY, new Vector2(0, 0));
+
+            var connections = cell.RelativeConnections;
+            Assert.IsNotNull(connections);
+            Assert.AreEqual(connections.Count, 0);
+
+            Assert.AreEqual(cell.Type, CellType.EMPTY);
+            Assert.AreEqual(cell.Position, new Vector2(0, 0));
+            Assert.AreEqual(cell.CellState, CellState.EMPTY);
+            Assert.AreEqual(cell.RelativeConnections.Count, 0);
+            Assert.AreEqual(cell.Rotation, 0);
+        }
+
+        [TestMethod]
+        public void CreateCell_CrossCell_CrossCell()
+        {
+            var cell = new Cell(CellType.CROSS, new Vector2(0, 0));
+
+            var connections = cell.RelativeConnections;
+            Assert.IsNotNull(connections);
+            Assert.AreEqual(connections.Count, 4);
+            Assert.AreEqual(connections[0], new Vector2(0, 1));
+            Assert.AreEqual(connections[1], new Vector2(0, -1));
+            Assert.AreEqual(connections[2], new Vector2(1, 0));
+            Assert.AreEqual(connections[3], new Vector2(-1, 0));
+
+            Assert.AreEqual(cell.Type, CellType.CROSS);
+            Assert.AreEqual(cell.Position, new Vector2(0, 0));
+            Assert.AreEqual(cell.CellState, CellState.EMPTY);
+            Assert.AreEqual(cell.RelativeConnections.Count, 4);
+            Assert.AreEqual(cell.Rotation, 0);
+        }
+
+        [TestMethod]
+        public void CreateCell_StraightCell_StraightCell()
+        {
+            var cell = new Cell(CellType.STRAIGHT, new Vector2(0, 0));
+
+            var connections = cell.RelativeConnections;
+            Assert.IsNotNull(connections);
+            Assert.AreEqual(connections.Count, 2);
+            Assert.AreEqual(connections[0], new Vector2(0, 1));
+            Assert.AreEqual(connections[1], new Vector2(0, -1));
+
+            Assert.AreEqual(cell.Type, CellType.STRAIGHT);
+            Assert.AreEqual(cell.Position, new Vector2(0, 0));
+            Assert.AreEqual(cell.CellState, CellState.EMPTY);
+            Assert.AreEqual(cell.RelativeConnections.Count, 2);
+            Assert.AreEqual(cell.Rotation, 0);
+        }
+    }
+
+    [TestClass]
     public sealed class CellFunctionsTest
     {
+        [TestMethod]
+        public void ClearField_EmptyField_EmptyField()
+        {
+            var cells = new List<List<Cell>>();
+            CellFunctions.ClearField(cells);
+            Assert.AreEqual(cells.Count, 0);
+        }
+
+        [TestMethod]
+        public void ClearField_FilledField_EmptyField()
+        {
+            var cells = new List<List<Cell>>();
+            CellFunctions.CreateField(cells, 2, 2);
+            CellFunctions.ClearField(cells);
+            Assert.AreEqual(cells.Count, 0);
+        }
+
         [TestMethod]
         public void CreateField_BasicSmallEmptyField_EmptySmallField()
         {
             var cells = new List<List<Cell>>();
+            CellFunctions.CreateField(cells, 2, 2);
+            Assert.AreEqual(cells.Count, 2);
+            Assert.AreEqual(cells[0].Count, 2);
+            Assert.AreEqual(cells[1].Count, 2);
+            Assert.AreEqual(cells[0][0].Type, CellType.EMPTY);
+            Assert.AreEqual(cells[0][1].Type, CellType.EMPTY);
+            Assert.AreEqual(cells[1][0].Type, CellType.EMPTY);
+            Assert.AreEqual(cells[1][1].Type, CellType.EMPTY);
+
+            Assert.AreEqual(cells[0][0].Position, new Vector2(0, 0));
+            Assert.AreEqual(cells[0][1].Position, new Vector2(0, 1));
+            Assert.AreEqual(cells[1][0].Position, new Vector2(1, 0));
+            Assert.AreEqual(cells[1][1].Position, new Vector2(1, 1));
+
+            Assert.AreEqual(cells[0][0].CellState, CellState.EMPTY);
+            Assert.AreEqual(cells[0][1].CellState, CellState.EMPTY);
+            Assert.AreEqual(cells[1][0].CellState, CellState.EMPTY);
+            Assert.AreEqual(cells[1][1].CellState, CellState.EMPTY);
+
+            Assert.AreEqual(cells[0][0].Rotation, 0);
+            Assert.AreEqual(cells[0][1].Rotation, 0);
+            Assert.AreEqual(cells[1][0].Rotation, 0);
+            Assert.AreEqual(cells[1][1].Rotation, 0);
+        }
+
+        [TestMethod]
+        public void CreateField_FilledFieldToSmallerField_SmallerFieldFromBig()
+        {
+            var cells = new List<List<Cell>>();
+            CellFunctions.CreateField(cells, 10, 10);
             CellFunctions.CreateField(cells, 2, 2);
             Assert.AreEqual(cells.Count, 2);
             Assert.AreEqual(cells[0].Count, 2);
